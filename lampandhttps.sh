bcfc9801942b43ba7258b987c71443b15bb7da5b
zer0 <zer0kin@null.net>
2018-10-15 14:34:05 -0700

    Adding -y argument for less user input.
    👍 Gotta make it easy.

--- lampandhttps.sh
+++ lampandhttps.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 sudo apt-get update
-sudo apt-get install apache2
-apt-get install nano
-sudo nano /etc/apache2/apache2.conf
-sudo systemctl restart apache2
-sudo apt-get install mysql-server
+sudo apt-get install apache2 -y
+apt-get install nano -y
+sudo nano /etc/apache2/apache2.conf 
+sudo systemctl restart apache2 
+sudo apt-get install mysql-server -y
 mysql_secure_installation
-sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
+sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql -y
 sudo nano /etc/apache2/mods-enabled/dir.conf
 sudo systemctl restart apache2
 sudo apt-get update
-sudo apt-get install software-properties-common
+sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:certbot/certbot
 sudo apt-get update
-sudo apt-get install python-certbot-apache 
+sudo apt-get install python-certbot-apache  -y
 sudo certbot --apache
